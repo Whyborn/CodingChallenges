ecb072d527dc12da62bc1fae679cdfe016282618
Whyborn <lachlan.whyborn@uqconnect.edu.au>
2021-12-15 00:02:26 +1000

    Fixed day 13- question info incomplete

--- AdventOfCode/2021/Day13/Day13.py
+++ AdventOfCode/2021/Day13/Day13.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../../')
 from get_input import get_data, split_on_lines
 import numpy as np
 
@@ -31,17 +33,31 @@ split_point = data.index('')
 dots = [tuple([int(var) for var in line.split(',')[::-1]]) for line in data[:split_point]]
 folds = data[split_point+1:]
 
-# Assume the size of the paper is the maximum of the x and y coordinates
-i_size, j_size = zip(*dots)
-i_size, j_size = max(i_size) + 1, max(j_size) + 1
+# Maybe base it on the fold locations?
+found_x_size = False
+found_y_size = False
+for fold in folds:
+    cmd_point = fold.index("=")
+    direction = fold[cmd_point-1]
+    fold_line = int(fold[cmd_point+1:])
+
+    if direction == "x" and not found_x_size:
+        found_x_size = True
+        j_size = 2 * fold_line + 1
+
+    elif direction == "y" and not found_y_size:
+        found_y_size = True
+        i_size = 2 * fold_line + 1
+
+    if found_x_size and found_y_size:
+        break
 
 paper = np.zeros((i_size, j_size), dtype = int)
 
 for index in dots:
     paper[index] = 1
 
-print(paper[39, 8])
-print(paper[39, :100])
+print(len(paper[paper == 1]))
 
 for num, fold in enumerate(folds):
     # Find location of the '='
@@ -54,12 +70,18 @@ for num, fold in enumerate(folds):
     if direction == "x":
         print(f"Folding along the vertical line x={fold_line}, with paper size {(m, n)}")
 
-        paper = np.logical_or(paper[:, :fold_line-1], paper[:, fold_line:])
+        paper = np.logical_or(paper[:, :fold_line], np.flip(paper[:, fold_line+1:], axis = 1))
 
     elif direction == "y":
-        print(f"Folding along the horizontal line y={fold_line}, with paper size ({m}, {n})")
+        print(f"Folding along the horizontal line y={fold_line}, with paper size {(m, n)}")
 
-        paper = np.logical_or(paper[:fold_line-1, :], paper[fold_line:, :])
+        paper = np.logical_or(paper[:fold_line, :], np.flip(paper[fold_line+1:, :], axis = 0))
 
     print(len(paper[paper == 1]))
 
+import matplotlib.pyplot as mpl
+
+mpl.figure()
+mpl.imshow(paper.astype('uint8'), interpolation = 'nearest')
+mpl.savefig("output.png")
+mpl.close()
